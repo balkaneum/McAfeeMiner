5d3070a60d8625f6c1ec8156d45fc96bd574f328
Uros <umitrovic1@gmail.com>
2018-11-19 20:23:23 +0100

    getting everything ready for production

--- createWinInstaller.js
+++ createWinInstaller.js
@@ -14,13 +14,13 @@ function getInstallerConfig () {
     const outPath = path.join(rootPath, 'installers')
 
     return Promise.resolve({
-       appDirectory: path.join(rootPath, 'release-builds', 'Safex1ClickMinigApp-win32-ia32'),
+       appDirectory: path.join(rootPath, 'release-builds', 'McAfee1ClickMinigApp-win32-ia32'),
        authors: 'Safex Developers',
        noMsi: true,
        outputDirectory: outPath,
-       exe: 'Safex1ClickMinigApp.exe',
-       setupExe: 'Safex1ClickMinigAppWindowsInstaller.exe',
-       setupIcon: 'public/images/icons/icon.ico',
+       exe: 'McAfee1ClickMinigApp.exe',
+       setupExe: 'McAfee1ClickMinigAppWindowsInstaller.exe',
+       setupIcon: 'public/images/icons/icon4.ico',
        skipUpdateIcon: true
    })
 }
